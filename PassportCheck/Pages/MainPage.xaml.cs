45a2edd64bbc260931bfb41077716c8c508a8306
Anton Martynenko <a.o.martynenko@gmail.com>
2023-05-04 13:00:18 +0100

    Created Windows: - MainWindow - Login/RegistrationWindow
    Created Pages:
    - MainPage
    - LoginPage
    -RegistrationPage

---
+++ PassportCheck/Pages/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassportCheck.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void datePickerSearch_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonResetSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dataGrid_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void buttonEditConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonAddConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDeleteSelected_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDeleteAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PassportCheck/Pages/RegPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassportCheck.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+
+        private void showPassword_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void showPassword_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
